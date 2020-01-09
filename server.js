7789819a351127ba3c96ed9a7028067ea2d0d428
Javier <javieralvarez82@live.com>
2020-01-08 18:39:33 -0800

    Add backend dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const server = express()
+const helmet = require('helmet')
+const cors = require('cors')
+
+//server middleware
+server.use(cors())
+server.use(helmet())
+server.use(express.json())
