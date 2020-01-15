97b94d708c4b91271705a3ea38d6e174a9e8a6f6
Javier Alavrez <javieralvarez82@live.com>
2020-01-14 23:59:33 -0800

    Create Database and database Schema

--- migrations/20200114235006_messageDB.js
+++ migrations/20200114235006_messageDB.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('message', table => {
+        table.integer('Id').primary()
+        table.datetime('dateTime').defaultTo(knex.fn.now());
+        table.string('Name', 100).notNullable();
+        table.string('Email').unique().notNullable();
+        table.text('Message').notNullable();
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('message')
+  };
